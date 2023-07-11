ea925ef5ab3993d275eb5fed237cb575ea4d75b9
pho3niix <hugo.costillah@gmail.com>
2023-07-10 22:53:05 -0600

    Added favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,15 @@ import Layout from "../components/Layout";
 import '../utils/global.css';
 import '../utils/normalize.css';
 import { MyContextProvider } from '../utils/MyContext';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
     return (
         <MyContextProvider>
+            <Head>
+                <title>Sticky Wall</title>
+                <link rel="icon" href="https://cdn.icon-icons.com/icons2/1859/PNG/512/checklist_117966.png" sizes="any" />
+            </Head>
             <Layout>
                 <Component {...pageProps} />
             </Layout>
